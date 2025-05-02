4c02cb3670bf82642041d3bbaeb1dfd2299b20a7
Luis Torres <luistorres@Luiss-MacBook-Pro.local>
2025-05-02 16:11:41 -0400

    mapas update and more

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ const navLinks = document.getElementById('nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+// mapas
+
+document.addEventListener("DOMContentLoaded", () => {
+  const targets = document.querySelectorAll(".map-container svg path, .map-container svg polygon");
+  targets.forEach(shape => {
+    shape.addEventListener("click", () => {
+      shape.classList.toggle("clicked");
+      const name = shape.getAttribute("id") || "Sin nombre";
+      alert(`Clic en: ${name}`);
+    });
+  });
+});
+
+
+
